45655b2ec75658133b6d2070e7a5447e82a1c53f
chrispowell23 <christian@dataprotocol.com>
2022-05-24 09:16:03 -0400

    First Commit

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
+import $ from "jquery"
 
 class Search extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      genres: []
-    };
+      genres: [],
+    }
   }
   getGenres() {
     //make an axios request in this component to get the list of genres from your endpoint GET GENRES
+    console.log("gotGenres")
+    $.ajax({
+      url: "/genres",
+      type: "GET",
+      success: (resp) => {
+        this.setState({ genres: resp.genres })
+      },
+    })
+  }
+
+  search() {
+    this.props.onSearch()
   }
 
   render() {
     return (
       <div className="search">
-        <button onClick={() => {this.props.swapFavorites()}}>{this.props.showFaves ? "Show Results" : "Show Favorites"}</button>
-        <br/><br/>
+        <button
+          onClick={() => {
+            this.props.swapFavorites()
+          }}
+        >
+          {this.props.showFaves ? "Show Results" : "Show Favorites"}
+        </button>
+        <br />
+        <br />
 
         {/* Make the select options dynamic from genres !!! */}
         {/* How can you tell which option has been selected from here? */}
@@ -25,12 +45,12 @@ class Search extends React.Component {
           <option value="thisway">This Way</option>
           <option value="thatway">That Way</option>
         </select>
-        <br/><br/>
+        <br />
+        <br />
 
         <button>Search</button>
-
       </div>
-    );
+    )
   }
 }
 
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'badmovies',
-  API_KEY: '',
-};
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "badmovies",
+  API_KEY: "d728f0b52369904673a5a9f9395d5814",
+}
